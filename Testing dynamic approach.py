8fdc675ee3372fc71ebd8b855beb5366fe06558f
magRubennet <47723008+magRubennet@users.noreply.github.com>
2023-11-26 12:42:57 -0800

    Add files via upload
    Work in progress PartB

---
+++ Testing dynamic approach.py
@@ -0,0 +1,49 @@
+# import itertools not used as well as combo
+
+
+def stock_maximization(M, values, stock):
+    memo = {}
+
+    def dp(index, remaining_money):
+        if index == 0 or remaining_money == 0:
+            return 0, 0
+
+        if (index, remaining_money) in memo:
+            return memo[(index, remaining_money)]
+
+        # Case 1: Exclude the current stock
+        exclude_stock, exclude_value = dp(index - 1, remaining_money)
+
+        # Case 2: Include the current stock if it fits within the budget
+        current_stock = stock[index - 1]
+        if current_stock <= remaining_money:
+            include_stock, include_value = dp(index - 1, remaining_money - current_stock)
+            include_value += values[index - 1]
+
+            # Choose the option with higher value or higher stock if values are equal
+            if include_value > exclude_value or (include_value == exclude_value and include_stock > exclude_stock):
+                result = include_stock + current_stock, include_value
+            else:
+                result = exclude_stock, exclude_value
+        else:
+            result = exclude_stock, exclude_value
+
+        memo[(index, remaining_money)] = result
+        return result
+
+    stock, values = dp(len(stock), M)
+    return stock, values
+
+# Testing example test cases
+
+
+N = 4
+Stocks_and_values = [[3, 2], [6, 3], [5, 3], [6, 7]]
+Amount = 10
+
+stock = [x[0] for x in Stocks_and_values]
+values = [x[1] for x in Stocks_and_values]
+
+result_stock, result_values = stock_maximization(Amount, None, values, stock)
+
+print("The best bang for your buck would be {} stocks for ${}.".format(result_stock, result_values))
